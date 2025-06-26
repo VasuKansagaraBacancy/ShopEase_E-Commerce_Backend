ddc145cddff1415b0b8b974d9ab519a58f40c2e7
VasuKansagaraBacancy <154701855+vasupatel414@users.noreply.github.com>
2025-06-26 15:30:24 +0530

    Update Folder Structure

--- ShopEase_E-Commerce.Infrastructure/obj/Debug/net8.0/ShopEase_E-Commerce.Infrastructure.AssemblyInfo.cs
+++ ShopEase_E-Commerce.Infrastructure/obj/Debug/net8.0/ShopEase_E-Commerce.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopEase_E-Commerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7dab7eac3160268e4bd476c9a9edc59e152f2f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopEase_E-Commerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopEase_E-Commerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
